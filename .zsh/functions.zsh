b3005f0247dff109bdc9e56c5b49b18992520c70
Artem Barchan <artemweb@me.com>
2016-03-30 20:57:44 +0300

    added new functions

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,59 +1,64 @@
 # Create a New Directory and Enter it
 md() {
-  mkdir -p "$1" && cd "$1"
+	mkdir -p "$1" && cd "$1"
 }
 
 # Show a Tree Structure of the Current Directory
 tree() {
-  find . -print | sed -e "s;[^/]*/;|____;g;s;____|;   |;g"
+	find . -print | sed -e "s;[^/]*/;|____;g;s;____|;   |;g"
 }
 
 # Download File via Curl
 get() {
-  curl -# -O "$1"
+	curl -# -O "$1"
 }
 
 # Recursively Remove All .DS_Store Files
 rmds() {
-  find . -name '.DS_Store' -type f -delete
+	find . -name '.DS_Store' -type f -delete
+}
+
+# Generate a Random Password
+genpass() {
+	openssl rand -base64 32 | tr -d '=/+' | cut -c 1-16 | tr -d '\n' | pbcopy
 }
 
 # Pack File or Directory in tar.gz
 pack() {
-  tar --exclude=.DS_Store -zcvf "$1".tar.gz "$1"
+	tar --exclude=.DS_Store -zcvf "$1".tar.gz "$1"
 }
 
 # Get Size of Image Using Sips
 imgsize() {
-  sips --getProperty pixelWidth --getProperty pixelHeight "$1"
+	sips --getProperty pixelWidth --getProperty pixelHeight "$1"
 }
 
 # Running DLNA server via MiniDLNA
 dlna_start() {
-  minidlnad -R \
-    -f ~/.config/minidlna/minidlna.conf \
-    -P ~/.config/minidlna/minidlna.pid
+	minidlnad -R \
+		-f ~/.config/minidlna/minidlna.conf \
+		-P ~/.config/minidlna/minidlna.pid
 }
 
 dlna_stop() {
-  pkill minidlnad
+	pkill minidlnad
 }
 
 # Shortcut for Fast Gulp Plugin Installation
 gi() {
-  npm install --save-dev gulp-"$1"
+	npm install --save-dev gulp-"$1"
 }
 
 # Prepare WordPress to Theme or Plugin Development
 wpgo() {
-  curl -# -O https://wordpress.org/latest.tar.gz
-  tar xzf latest.tar.gz
-  mv wordpress/* .
-  rm -rf \
-    wordpress \
-    latest.tar.gz \
-    license.txt \
-    readme.html \
-    wp-content/plugins/{akismet,hello.php} \
-    wp-content/themes/twenty*
+	curl -# -O https://wordpress.org/latest.tar.gz
+	tar xzf latest.tar.gz
+	mv wordpress/* .
+	rm -rf \
+		wordpress \
+		latest.tar.gz \
+		license.txt \
+		readme.html \
+		wp-content/plugins/{akismet,hello.php} \
+		wp-content/themes/twenty*
 }
