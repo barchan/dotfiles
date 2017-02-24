6fedfd6a84d20d6d9137bfec80332c47a15a4cf1
Artem Barchan <artemweb@me.com>
2017-02-24 22:42:48 +0200

    New version

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -3,4 +3,4 @@ export LC_ALL=ru_RU.UTF-8
 export HISTSIZE=2000
 export SAVEHIST=2000
 export HISTFILE=~/.zsh_history
-export PATH=/usr/local/bin:$PATH
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
